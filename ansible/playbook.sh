054a170975fc1280616f2c81421b4aee54297367
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2020-11-14 07:26:59 +0200

    Местим се в AWS за стабилност

--- ansible/playbook.sh
+++ ansible/playbook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i inventory --private-key=secret/id_rsa.private --vault-password=getpass.sh playbook.yaml $@
+ansible-playbook -i inventory --private-key=secret/zhiva-mrezha-aws.pem --vault-password=getpass.sh playbook.yaml "$@"
