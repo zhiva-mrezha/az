462c5994380b0a18ef5b7a264296718b81574119
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2020-12-07 08:09:55 +0200

    Работещ phpList за седмичник

---
+++ ansible/roles/phplist/files/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$database_host = 'localhost';
+$database_name = '{{ phplist_db_name }}';
+$database_user = '{{ phplist_db_user }}';
+$database_password = '{{ phplist_db_password }}';
+$pageroot = '';
+define ("TEST",0);
+define('PHPMAILERHOST', 'smtp.eu.mailgun.org');
+$phpmailer_smtpuser = 'postmaster@{{ mail_domain }}';
+$phpmailer_smtppassword = '{{ mail_smtppassword }}';
+define("PHPMAILERPORT",'465');
+define("PHPMAILER_SECURE",'ssl');
+define('PHPMAILER_SMTP_DEBUG', 3);
